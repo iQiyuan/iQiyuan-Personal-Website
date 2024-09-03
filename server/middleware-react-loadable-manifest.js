662663484805ec9908f77152d34ea55359aaf312
iQiyuan <qiyuann@outlook.com>
2024-09-03 01:41:47 -0700

    Updates

---
+++ server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST='{"pages\\\\index.tsx -> ../components/Sections/Header":{"id":2938,"files":["static/chunks/681.f4f32370dbbd2e6e.js","static/chunks/938.8fe8f3052ec01899.js"]}}';
---
+++ static/5EZ8M3xRgnthmmasB5tbz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/16-17fed17dd9a1454c.js","static/chunks/pages/index-9f492dcea0f5da37.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
