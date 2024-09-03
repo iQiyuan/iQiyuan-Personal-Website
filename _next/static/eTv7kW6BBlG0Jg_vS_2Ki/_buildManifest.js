d3be0c0c5df1e30f9f83ab088b77174536a7690d
iQiyuan <qiyuann@outlook.com>
2024-09-03 02:11:31 -0700

    Updates

---
+++ _next/static/eTv7kW6BBlG0Jg_vS_2Ki/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/16-7020da4e60285008.js","static/chunks/pages/index-9f492dcea0f5da37.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST='{"pages\\\\index.tsx -> ../components/Sections/Header":{"id":2938,"files":["static/chunks/681.f4f32370dbbd2e6e.js","static/chunks/938.8fe8f3052ec01899.js"]}}';
